bb0d111862586ba2fd1ac6f9b82f1e085795f2b3
Ap0ll0 <Ap0ll0Media@protonmail.com>
2024-05-26 23:08:38 -0700

    a couple more things I guess

--- project2/project1.py
+++ project2/project1.py
@@ -23,3 +23,7 @@ def load(file):
     except IOError as e_io:
         print(e_io + " Error Opening " + file, file=sys.stderr)
         sys.exit(1)
+
+
+if __name__ == '__main__':
+    print("congrats\n")
