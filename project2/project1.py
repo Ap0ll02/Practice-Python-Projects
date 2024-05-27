63c9c83755ac2c903e71c46d4c51f6bc0494b760
Ap0ll0 <Ap0ll0Media@protonmail.com>
2024-05-26 22:56:04 -0700

    updates to pylint

--- project2/project1.py
+++ project2/project1.py
@@ -4,6 +4,7 @@ Loading A Python File.
 Author: Jack Ratermann
 Date: May 26, 2024
 """
+import locale
 import sys
 
 
@@ -15,10 +16,10 @@ def load(file):
     Returns: a list from the file
     """
     try:
-        with open(file) as in_file:
+        with open(file, encoding=locale.getpreferredencoding()) as in_file:
             loaded_text = in_file.read().strip().split('\n')
             loaded_text = [x.lower() for x in loaded_text]
             return loaded_text
     except IOError as e_io:
-        print("{} Error Opening {}".format(e_io, file), file=sys.stderr)
+        print(e_io + " Error Opening " + file, file=sys.stderr)
         sys.exit(1)
